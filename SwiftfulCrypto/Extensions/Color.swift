5339e427ade4d30b0f52e6ade062c56f42b03b6b
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-15 23:21:27 -0700

    Set up Xcode Project and add color themes

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Chi-Hsien Wu on 7/15/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
