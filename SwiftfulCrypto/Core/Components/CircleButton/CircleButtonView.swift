74c0a79c2bda1a9b06e0d0a8b83bdfe27570ae26
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-15 23:27:50 -0700

    Create CircleButton and header of HomeView

---
+++ SwiftfulCrypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  CircleButtonView.swift
+//  SwiftfulCrypto
+//
+//  Created by Chi-Hsien Wu on 7/15/24.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    
+    let iconName: String
+    
+    var body: some View {
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundColor(Color.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+                Circle()
+                    .foregroundColor(Color.theme.background)
+            )
+            .shadow(
+                color: Color.theme.accent.opacity(0.25),
+                radius: 10, x: 0, y: 0)
+            .padding()
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    Group {
+        CircleButtonView(iconName: "info")
+        CircleButtonView(iconName: "plus")
+    }
+}
---
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  SwiftfulCrypto
+//
+//  Created by Chi-Hsien Wu on 7/15/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false 
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
