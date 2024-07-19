a21a218ce23a633811d7438ead00ad2cc6b661ed
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-19 16:36:48 -0700

    Create CoinImageView, CoinImageViewModel and CoinImageService to download coin images

---
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageService.swift
+//  SwiftfulCrypto
+//
+//  Created by Chi-Hsien Wu on 7/19/24.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+      getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+    
+}
