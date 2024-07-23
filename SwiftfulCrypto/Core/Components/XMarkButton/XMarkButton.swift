a8da722c8b5a2cee10d073738a5c6d92e52400bd
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-22 21:25:36 -0700

    Create PortfolioView

---
+++ SwiftfulCrypto/Core/Components/XMarkButton/XMarkButton.swift
@@ -0,0 +1,20 @@
+//
+//  XMarkButton.swift
+//  SwiftfulCrypto
+//
+//  Created by Chi-Hsien Wu on 7/22/24.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.headline)
+    }
+}
+
+#Preview {
+    XMarkButton()
+}
